e38a9f297f7251294c91e73af90b928b0254248f
Alexander Lisachenko <lisachenko.it@gmail.com>
2026-07-30 20:55:32 +0000

    docs: use the current year in the copyright header of new files

--- tests/EngineConstantsTest.php
+++ tests/EngineConstantsTest.php
@@ -3,7 +3,7 @@
 /**
  * Z-Engine framework
  *
- * @copyright Copyright 2019, Lisachenko Alexander <lisachenko.it@gmail.com>
+ * @copyright Copyright 2026, Lisachenko Alexander <lisachenko.it@gmail.com>
  *
  * This source file is subject to the license that is bundled
  * with this source code in the file LICENSE.
